6c593d1fdea9ee56a02ca7611506e8474e2f5348
aquirosp2705-cloud <aquirosp2705@gmail.com>
2026-04-21 12:41:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import List
 
 app = FastAPI()
 
-data = []
-
-class Movimiento(BaseModel):
-    tarjeta: str
-    fecha: str
-    concepto: str
-    pago: float
-
 @app.get("/")
 def root():
-    return {"ok": True}
+    return {"ok": True, "mensaje": "API funcionando"}
 
-@app.get("/movimientos")
-def listar():
-    return data
-
-@app.post("/movimientos")
-def guardar(mov: Movimiento):
-    data.append(mov.dict())
-    return {"ok": True, "data": mov}
+@app.get("/ping")
+def ping():
+    return {"pong": True}
